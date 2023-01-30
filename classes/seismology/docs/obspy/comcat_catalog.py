451683061d4a392d25e7b774670cb66bd83b1a6d
Weiqiang <zhuwq0@gmail.com>
2023-01-30 15:54:16 -0800

    update download

--- classes/seismology/docs/obspy/comcat_catalog.py
+++ classes/seismology/docs/obspy/comcat_catalog.py
@@ -10,6 +10,7 @@ from libcomcat.search import get_event_by_id
 from libcomcat.search import search
 from libcomcat.classes import DetailEvent, SummaryEvent
 from libcomcat.utils import HEADERS, TIMEOUT
+TIMEOUT = 60 * 60
 
 from obspy.io.quakeml.core import Unpickler
 
@@ -19,11 +20,70 @@ from pathlib import Path
 import requests
 import json
 import functools
+import multiprocessing as mp
+from tqdm import tqdm
+import time
 
+# %%
+def parse_pick(pick, type='pick'):
+    tmp_pick = {}
+    if type == 'pick':
+        tmp_pick["resource_id"] = pick.resource_id
+        tmp_pick["network"] = pick.waveform_id.network_code
+        tmp_pick["station"] = pick.waveform_id.station_code
+        tmp_pick["channel"] = pick.waveform_id.channel_code
+        tmp_pick["location"] = pick.waveform_id.location_code
+        tmp_pick["phase_time"] = pick.time.datetime.isoformat(timespec='milliseconds')
+        tmp_pick["oneset"] = pick.onset
+        tmp_pick["polarity"] = pick.polarity
+        tmp_pick["evaluation_mode"] = pick.evaluation_mode
+        tmp_pick["evaluation_status"] = pick.evaluation_status
+    elif type == 'arrival':
+        tmp_pick["resource_id"] = pick.pick_id
+        tmp_pick["phase_type"] = pick.phase
+        tmp_pick["azimuth"] = pick.azimuth
+        tmp_pick["distance"] = pick.distance
+        tmp_pick["takeoff_angle"] = pick.takeoff_angle
+        tmp_pick["time_residual"] = pick.time_residual
+        tmp_pick["time_weight"] = pick.time_weight
+        tmp_pick["time_correction"] = pick.time_correction
+    else:
+        raise ValueError("type must be 'pick' or 'arrival'")
+
+    return tmp_pick
+
+def add_pick(pick_dict, pick):
+
+    if pick["resource_id"] not in pick_dict:
+        pick_dict[pick["resource_id"]] = pick
+    else:
+        pick_dict[pick["resource_id"]].update(pick)
+    
+    return pick_dict
 
 def download_catalog(event_id, event_path, phase_path, raw_event_path, raw_phase_path):
 
-    detail = get_event_by_id(event_id, includesuperseded=True)
+    if (event_path/f'{event_id}.json').exists():
+        print(f"{event_id}.json already exists")
+        return
+
+    try:
+        detail = get_event_by_id(event_id, includesuperseded=True)
+    except Exception as e:
+        print(f"{event_id}: {e}")
+        # time.sleep(1)
+        return
+
+    # tires = 0
+    # max_tires = 10
+    # while tires < max_tires:
+    #     try:
+    #         detail = get_event_by_id(event_id, includesuperseded=True)
+    #         break
+    #     except Exception as e:
+    #         print(f"{tires}: {e}")
+    #         tires += 1
+    #         time.sleep(60)
 
     # %%
     with open(raw_event_path / f"{event_id}.pkl", "wb") as f:
@@ -33,43 +93,6 @@ def download_catalog(event_id, event_path, phase_path, raw_event_path, raw_phase
     # with open(raw_event_path / f"{event_id}.pkl", "rb") as f:
     #     x = pickle.load(f)
     # print(x)
-
-    # %%
-    def parse_pick(pick, type='pick'):
-        tmp_pick = {}
-        if type == 'pick':
-            tmp_pick["resource_id"] = pick.resource_id
-            tmp_pick["network"] = pick.waveform_id.network_code
-            tmp_pick["station"] = pick.waveform_id.station_code
-            tmp_pick["channel"] = pick.waveform_id.channel_code
-            tmp_pick["location"] = pick.waveform_id.location_code
-            tmp_pick["phase_time"] = pick.time.datetime.isoformat(timespec='milliseconds')
-            tmp_pick["oneset"] = pick.onset
-            tmp_pick["polarity"] = pick.polarity
-            tmp_pick["evaluation_mode"] = pick.evaluation_mode
-            tmp_pick["evaluation_status"] = pick.evaluation_status
-        elif type == 'arrival':
-            tmp_pick["resource_id"] = pick.pick_id
-            tmp_pick["phase_type"] = pick.phase
-            tmp_pick["azimuth"] = pick.azimuth
-            tmp_pick["distance"] = pick.distance
-            tmp_pick["takeoff_angle"] = pick.takeoff_angle
-            tmp_pick["time_residual"] = pick.time_residual
-            tmp_pick["time_weight"] = pick.time_weight
-            tmp_pick["time_correction"] = pick.time_correction
-        else:
-            raise ValueError("type must be 'pick' or 'arrival'")
-
-        return tmp_pick
-
-    def add_pick(pick_dict, pick):
-
-        if pick["resource_id"] not in pick_dict:
-            pick_dict[pick["resource_id"]] = pick
-        else:
-            pick_dict[pick["resource_id"]].update(pick)
-        
-        return pick_dict
 
     # %%
     pick_df = []
@@ -118,6 +141,7 @@ def download_catalog(event_id, event_path, phase_path, raw_event_path, raw_phase
         pick_df.append(pd.DataFrame.from_dict(pick_dict, orient='index'))
 
     pick_df = pd.concat(pick_df)
+    pick_df.sort_values(by=["network","station","location","channel","phase_type"], inplace=True)
 
     # %%
     pick_df.to_csv(phase_path/f'{event_id}.csv', index=False)
@@ -132,24 +156,34 @@ def download_catalog(event_id, event_path, phase_path, raw_event_path, raw_phase
 
 
     # %%
-    origins_fc = detail.getProducts('focal-mechanism')
-    for origin in origins_fc:
-        for k in origin.properties:
-            # print(k, origin[k])
-            event_dict[k] = origin[k]
+    try:
+        origins_fc = detail.getProducts('focal-mechanism')
+        for origin in origins_fc:
+            for k in origin.properties:
+                # print(k, origin[k])
+                event_dict[k] = origin[k]
+    except Exception as e:
+        # print(f"{e}")
+        pass
 
     # %%
-    origins_mt = detail.getProducts('moment-tensor')
-    for origin in origins_mt:
-        for k in origin.properties:
-            # print(k, origin[k])
-            event_dict[k] = origin[k]
+    try:
+        origins_mt = detail.getProducts('moment-tensor')
+        for origin in origins_mt:
+            for k in origin.properties:
+                # print(k, origin[k])
+                event_dict[k] = origin[k]
+    except Exception as e:
+        # print(f"{e}")
+        pass
 
     # %%
     # event_df = pd.DataFrame.from_dict(event_dict, orient='index').T
     # event_df.to_csv(event_path/f'{event_id}.csv', index=False)
     with open(event_path/f'{event_id}.json', 'w') as f:
         json.dump(event_dict, f, indent=2)
+
+    # time.sleep(1)
 
 # %%
 
@@ -169,9 +203,27 @@ if __name__ == "__main__":
         raw_phase_path.mkdir()
 
     # %%
-    event_id = 'nc73201181'
+    # event_id = 'nc73201181'
+    # download_catalog(event_id, phase_path=phase_path, event_path=event_path, raw_event_path=raw_event_path, raw_phase_path=raw_phase_path)
+
+    # %%
+    with open("event_id.json", "r") as f:
+        tmp = json.load(f)
+    event_ids = ["nc"+tmp[k] for k in tmp]
+    # event_ids = sorted(event_ids)[::-1]
+    
     download_catalog_ = functools.partial(download_catalog, phase_path=phase_path, event_path=event_path, raw_event_path=raw_event_path, raw_phase_path=raw_phase_path)
-    download_catalog_(event_id)
+    # download_catalog_(event_id)
+
+    for _ in range(10):
+        for event_id in tqdm(event_ids):
+            download_catalog_(event_id)
+        #     break
+
+    # ncpu = mp.cpu_count()
+    # ncpu = 4
+    # with mp.Pool(ncpu) as p:
+    #     p.map(download_catalog_, event_ids)
 
 
 # %%
